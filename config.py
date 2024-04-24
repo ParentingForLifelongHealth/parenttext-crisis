695d374ea06c0fe8edd1b0b6ee7d762805b6d8d7
Georg Osang <geoo89@gmail.com>
2024-04-24 17:58:21 +0200

    Update config format

--- config.py
+++ config.py
@@ -1,36 +1,51 @@
-# Data sources, IDs of Google Sheets where the core date is stored.
-# Specific for PCC.
-localised_sheets = "1M3ohkdFNWzYQdMBdlnPP2_pi3RttKGXAvCUBgYLvuHA"
 
-# Shared with all deployments.
-# Multiple content index for different types of content.
-T_content = "1hcH8pFdiHZN0UvZgyv3Zht9ARBTx-VXhNBI2o8L7fHU"
-N_onboarding_data = "1NujmHWbalM74U0Yl370MABtoYp6vfusUsrpq9a80n2Q"
-T_onboarding = "1Sl0Jl_N4cGQi2INmE_EnX_aYUMUrUB6cKbuWVPzirtY"
-C_ltp_activities ="1P8OmAMo7_KPVDGSBScRB6ml2e4psCeKS3deG75NFllw"
-C_modules_all_ages = "1kGl23QMmUHPUamKxMkNqivYi2zESE7hKX6xkL-WnEM0"
-N_safeguarding_data = "1da7Kiw8KJXc026Ydq0lp52m7nP3TjoyWTHuxa74u5Tg"
-T_safeguarding = "1bWOyM5yShTTJSaxwqRCrjUzkwbp7DF6_nSF_96YcZ2c"
-T_delivery = "1q6E2c4Bg_UvqTmhxAsTIQngwAtj0aFoqu8wsPHnqmaU"
-N_delivery_data_response = "1W5Z0usyFcxZo85nXjSbjUj0-mjf7bG646w0BCwlO1pU"
-T_menu = "1lIiFjZKS0eXzzo6XwDdqYv4e1A73WFCpWZg5ju-tCZE"
-N_menu_data_response = "1Mg1MuS3p2FNMVJl9Qu9ouU9rjzOrPiF3HWleMKHPK3Y"
-N_menu_data_common = "1maT0rZGZjm1cyqyr1U6wI3HULiVVyTEV0xqjkkXki8c"
+sheet_names = {
+    # Data sources, IDs of Google Sheets where the core date is stored.
+    # Specific for PCC.
+    "localised_sheets" : "1M3ohkdFNWzYQdMBdlnPP2_pi3RttKGXAvCUBgYLvuHA",
+
+    # Shared with all deployments.
+    # Multiple content index for different types of content.
+    "T_content" : "1hcH8pFdiHZN0UvZgyv3Zht9ARBTx-VXhNBI2o8L7fHU",
+    "N_onboarding_data" : "1NujmHWbalM74U0Yl370MABtoYp6vfusUsrpq9a80n2Q",
+    "T_onboarding" : "1Sl0Jl_N4cGQi2INmE_EnX_aYUMUrUB6cKbuWVPzirtY",
+    "C_ltp_activities" :"1P8OmAMo7_KPVDGSBScRB6ml2e4psCeKS3deG75NFllw",
+    "C_modules_all_ages" : "1kGl23QMmUHPUamKxMkNqivYi2zESE7hKX6xkL-WnEM0",
+    "N_safeguarding_data" : "1da7Kiw8KJXc026Ydq0lp52m7nP3TjoyWTHuxa74u5Tg",
+    "T_safeguarding" : "1bWOyM5yShTTJSaxwqRCrjUzkwbp7DF6_nSF_96YcZ2c",
+    "T_delivery" : "1q6E2c4Bg_UvqTmhxAsTIQngwAtj0aFoqu8wsPHnqmaU",
+    "N_delivery_data_response" : "1W5Z0usyFcxZo85nXjSbjUj0-mjf7bG646w0BCwlO1pU",
+    "T_menu" : "1lIiFjZKS0eXzzo6XwDdqYv4e1A73WFCpWZg5ju-tCZE",
+    "N_menu_data_response" : "1Mg1MuS3p2FNMVJl9Qu9ouU9rjzOrPiF3HWleMKHPK3Y",
+    "N_menu_data_common" : "1maT0rZGZjm1cyqyr1U6wI3HULiVVyTEV0xqjkkXki8c",
+
+    # Google Sheet ID containing AB testing data. (pre-translation edits)
+    # Same for all deployments.
+    "ab_testing_sheet_ID" : "1i_oqiJYkeoMsYdeFOcKlvvjnNCEdQnZlsm17fgNvK0s",
+    # Crisis specific.
+    "localisation_sheet_ID" : "1cyPKUKEkAgaxZMQAVdLqDt9OBEjYXHV2ciIzGKdRfd0", #remove??
+
+    # Google Sheet ID containing dict edits data. (post-translation edits)
+    # Same for all deployments.
+    "eng_edits_sheet_ID" : "1Ab8H_s26EuOiS4nZ6HGADjD4CZw55586LL66fl8tEWI",
+    # Crisis specific.
+    "transl_edits_sheet_ID" : "1QxkFWdy56WDHopeHysRVDpEJdDA_b_dKFVQqaMCftrU"
+}
 
 creation_spreadsheet_ids = [
-    N_onboarding_data,
-    T_onboarding,
-    C_ltp_activities,
-    C_modules_all_ages,
-    T_content,
-    N_safeguarding_data,
-    T_safeguarding,
-    N_delivery_data_response,
-    T_delivery,
-    N_menu_data_common,
-    N_menu_data_response,
-    T_menu,
-    localised_sheets
+    "N_onboarding_data",
+    "T_onboarding",
+    "C_ltp_activities",
+    "C_modules_all_ages",
+    "T_content",
+    "N_safeguarding_data",
+    "T_safeguarding",
+    "N_delivery_data_response",
+    "T_delivery",
+    "N_menu_data_common",
+    "N_menu_data_response",
+    "T_menu",
+    "localised_sheets"
 ]
 
 # Data used when modifying expiration times.
@@ -83,18 +98,6 @@ special_words = "./edits/special_words.json"
 count_threshold = "3"
 length_threshold = "18"
 
-# Google Sheet ID containing AB testing data.
-# Same for all deployments.
-ab_testing_sheet_ID = "1i_oqiJYkeoMsYdeFOcKlvvjnNCEdQnZlsm17fgNvK0s"
-# Crisis specific.
-localisation_sheet_ID = "1cyPKUKEkAgaxZMQAVdLqDt9OBEjYXHV2ciIzGKdRfd0" #remove??
-
-# Google Sheet ID containing dict edits data.
-# Same for all deployments.
-eng_edits_sheet_ID = "1Ab8H_s26EuOiS4nZ6HGADjD4CZw55586LL66fl8tEWI"
-# Crisis specific.
-transl_edits_sheet_ID = "1QxkFWdy56WDHopeHysRVDpEJdDA_b_dKFVQqaMCftrU"
-
 # Names of redirect flows to be modified as part of safeguarding process.
 redirect_flow_names = (
     '['
@@ -126,7 +129,7 @@ def create_config():
             # "flow_file": {
             #     "format": "json",
             #     "files_dict": {
-            #         "flows": "path/to/file/input_flows.json",
+            #         "flows": "excel_files/parenttext_all_1_load_from_sheets.json",
             #     }
             # },
             "flow_definitions": {
@@ -141,16 +144,16 @@ def create_config():
                 "format": "sheets",
                 "subformat": "google_sheets",
                 "files_list": [
-                    ab_testing_sheet_ID,
-                    localisation_sheet_ID,
+                    "ab_testing_sheet_ID",
+                    "localisation_sheet_ID",
                 ],
             },
             "edits_posttranslation": {
                 "format": "sheets",
                 "subformat": "google_sheets",
                 "files_list": [
-                    transl_edits_sheet_ID,
-                    eng_edits_sheet_ID,
+                    "transl_edits_sheet_ID",
+                    "eng_edits_sheet_ID",
                 ],
             },
             "translation": {
@@ -192,7 +195,7 @@ def create_config():
                 "format": "sheets",
                 "subformat": "google_sheets",
                 # TODO: This is for WashText, put the correct ID here for crisis
-                "files_list": {"1TJ1YVSu87ubc5GvGea2hfRh4v234OyJUccQnZ90VUP4"}
+                "files_list": ["1TJ1YVSu87ubc5GvGea2hfRh4v234OyJUccQnZ90VUP4"]
             },
         },
         "steps": [
